7f266da99b8f5c8ca64c9fff8199a6bdf7425721
happysalada <raphael.megzari@gmail.com>
2017-11-05 19:26:47 +0900

    fix config syntax error

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :chisanchisho, ChisanchishoWeb.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Configure your database
-config :chisanchisho, ChisanchishoWeb.Repo,
+config :chisanchisho, Chisanchisho.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
